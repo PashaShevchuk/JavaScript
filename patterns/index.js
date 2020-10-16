316bd2dc6d0da53cfadecff6bb7e1512e64755d9
PashaShevchuk <pasha608@gmail.com>
2020-10-16 19:53:05 +0300

    Add adapter pattern

--- patterns/index.js
+++ patterns/index.js
@@ -222,6 +222,58 @@ console.log(mysql.getData());
 // *** Structural Design Patterns ***
 //______________________________________________________________________________________________________________________
 // 1. Adapter
+// Adapter – это структурный паттерн проектирования, который позволяет объектам с несовместимыми интерфейсами работать вместе.
+// Підходить для рефакторингу коду та переходу на нові версії.
+
+class OldCalc {
+  operations(t1, t2, operation) {
+    switch (operation) {
+      case 'add':
+        return t1 + t2;
+      case 'sub':
+        return t1 - t2;
+      default:
+        return NaN;
+    }
+  }
+}
+
+class NewCalc {
+  add(t1, t2) {
+    return t1 + t2;
+  }
+
+  sub(t1, t2) {
+    return t1 - t2;
+  }
+}
+
+class CalcAdapter {
+  constructor() {
+    this.calc = new NewCalc();
+  }
+
+  operations(t1, t2, operation) {
+    switch (operation) {
+      case 'add':
+        return this.calc.add(t1, t2);
+      case 'sub':
+        return this.calc.sub(t1, t2);
+      default:
+        return NaN;
+    }
+  }
+}
+
+const oldCalc = new OldCalc();
+console.log(oldCalc.operations(5, 3, 'add')); // 8
+
+const newCalc = new NewCalc();
+console.log(newCalc.add(5, 3)); // 8
+
+const adapter = new CalcAdapter(); // старий інтерфейс, але можемо використовувати новий функціонал
+console.log(adapter.operations(5, 3, 'add')); // 8
+
 //______________________________________________________________________________________________________________________
 // 2. Decorator
 //______________________________________________________________________________________________________________________
